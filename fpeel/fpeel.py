084bf7d8341ae6edce8590f6d1a0a27d2af100ff
Joe Becher <jwbecher@drazisil.com>
2019-08-12 10:23:04 -0400

    add get_file_sig and is_ar_archive

--- fpeel/fpeel.py
+++ fpeel/fpeel.py
@@ -1,7 +1,42 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Peel a file.')
+parser.add_argument('file_path', help='the file path to act on')
+
+
+def get_file_sig(file_path, num_chars):
+    try:
+        file_stat = os.stat(file_path)
+    except FileNotFoundError as e:
+        raise e
+
+    fo = open(file_path, 'rb')
+    bytes = fo.read(num_chars)
+    fo.close()
+    return bytes
+
+
+def is_ar_archive(file_sig):
+    n = 2
+    [file_sig[i:i + n] for i in range(0, len(file_sig), n)]
+    # The file signature of an ar archive - https://en.wikipedia.org/wiki/Ar_(Unix)
+    magic_sig = set([0x21, 0x3C, 0x61, 0x72, 0x63, 0x68, 0x3E, 0x0a])
+    return magic_sig == set(file_sig)
+
+
 def say_hello():
     return 'Like an onion.'
 
 
 if __name__ == "__main__":
     print(say_hello())
-    pass
+    args = parser.parse_args()
+    try:
+        file_sig = get_file_sig(args.file_path, 8)
+    except FileNotFoundError as e:
+        print('File {} not found, please check it exists.'.format(
+            args.file_path))
+        quit(-1)
+    print(is_ar_archive(file_sig))
--- test/test_fpeel.py
+++ test/test_fpeel.py
@@ -1,4 +1,21 @@
-from fpeel.fpeel import say_hello
+from fpeel.fpeel import is_ar_archive, say_hello
+
+
+def test_is_ar_archive_passes():
+    assert is_ar_archive([0x21, 0x3C, 0x61, 0x72, 0x63, 0x68, 0x3E,
+                          0x0a]) == True
+
+
+def test_is_ar_archive_fails():
+    assert is_ar_archive([
+        0x21,
+        0x3C,
+        0x61,
+        0x72,
+        0x63,
+        0x68,
+        0x3E,
+    ]) == False
 
 
 def test_say_hello():
